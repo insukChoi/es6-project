ad2d641be786eb5b4b332da70ed0a2f82c1bf8ad
Insuk Choi <insuk@Insukui-MacBook-Pro.local>
2018-10-14 13:42:29 +0900

    vue 연동 및 jest 테스트

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class Server {
     }
     config() {
         this.app.set("views", path.join(__dirname, "views"));
-        this.app.set("view engine", "jade");
+        this.app.set("view engine", "pug");
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(express.static(path.join(__dirname, "public")));
